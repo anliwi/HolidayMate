699f49079cbf042d54bf816cff2e621eb70ef364
anliwi <alwirth@posteo.de>
2021-11-22 10:06:54 +0100

    Dictionary_v1

---
+++ QuestDictionaryHolidayMate-Copy1.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+Quest_HM = {
+    "City" : "Do you want to explore a city?",
+    "Countryside" : "Do you want to see the countryside?",
+    "Seaside" : "Do you want to stay by the ocean?",
+    "River" : "Fancy a river in the sourrindings?",
+    "Mountains" : "Do you want to stay in or close by the mountains?",
+    "Hotel" : "Fancy the comforts of a hotel?",
+    "Hostel" : "Prefer the sociability and budget choice of a hostel?",
+    "Train" : "Are you in for a train ride?",
+    "Train+Bike" : "Want to mix comfort and activity - taking a train-bike combo to arrive?",
+    "Bus" : "Would you be willing to take the bus to arrive at your destination?",
+    "Explore Nature" : "Are you dreaming of exploring nature during your holiday?",
+    "Food" : "Does the destination need to satisfy the foodie in you?",
+    "Culture" : "Is culture an essential part of your trip?",
+    "Party" : "Wanna let your party animal loose during the trip?",
+    "Relaxing" : "Is your number one priority for this holiday relaxing?",
+    "Active Holiday" : "Active holiday is a must for you?"
+}
+
+
+# In[9]:
+
+
+print(Quest_HM['City'])
+
+
+# In[10]:
+
+
+print(Quest_HM)
+
+
+# In[ ]:
+
+
+
+
