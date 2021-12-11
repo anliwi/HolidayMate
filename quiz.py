fe7917597e560b38cba567936632114665fd7fb3
anliwi <alwirth@posteo.de>
2021-12-11 11:49:40 +0100

    woi_com

--- quiz.py
+++ quiz.py
@@ -1,6 +1,5 @@
 import random
 from tkinter import *
-from tkinter import ttk
 
 
 from decision_algorithm import decTree
@@ -10,7 +9,7 @@ import QuestDictionaryHolidayMate
 Quest_HM = QuestDictionaryHolidayMate.Quest_HM
 
 D = decTree()
-df = pd.read_csv("Database Holiday Mate - Sheet1.csv")
+df = pd.read_csv("/home/czlowiek/Documents/Python/Database_Holiday_Mate.csv")
 D.fit(df, maxDepth = 6)
 print(D.getTree())
 print("\n\n\n\n\n\n")
@@ -36,37 +35,33 @@ class Quiz:
         clear()
         self.a1="yes"
         self.a2="no"
-<<<<<<< Updated upstream
-        self.antw1 = Button(window, text="yes",font=("Arial",14), command=lambda: self.SetResponse(1))
-        self.antw2 = Button(window, text="no",font=("Arial",14), command=lambda: self.SetResponse(0)) 
-        self.naechste = Button(window,text="Next",font=("Arial",14),command=self.Question) # STILL OPEN: adjust to: only works if a button was clicked
-        self.lock=False
-=======
         self.answ1 = Button(window, text="Yes",font=("Arial",14), command=lambda: self.SetResponse(1))
-        self.answ2 = Button(window, text="No",font=("Arial",14), command=lambda: self.SetResponse(0)) 
-        self.next = Button(window,text="Next",font=("Arial",14),command=self.Question) 
+        self.answ2 = Button(window, text="No",font=("Arial",14), command=lambda: self.SetResponse(0))
+        self.clearing = Button(window,text="Clear",font=("Arial",14), command=lambda: self.QuestionClearer())
+        self.next = Button(window,text="Next",font=("Arial",14),command=self.Question) # STILL OPEN: adjust to: only works if a button was clicked
         self.lock = False
->>>>>>> Stashed changes
         self.response = None
         self.nodes = []
         self.Question()
 
 
     def Question(self): 
-        self.naechste.grid(column=0,row=3,pady=5)    
+        self.next.grid(column=0,row=3,pady=5)    
+        self.answ1.grid(column=0,row=1,pady=(8,5))
+        self.answ2.grid(column=0,row=2,pady=5)
+        self.clearing.grid(column=0, row=4,pady=(8,5))
         c, nn = D.getNextNode(self.nodes)
         if c == 0:
             self.lock = False
-            questiontext = (str(Quest_HM[nn]))  
+            questiontext = (Quest_HM[nn], "?") # here we still need a nice display for the question 
             question = Text(window, font=("Arial",14), width=40, height=2)
             question.insert(END,questiontext)
             question.grid(column=0, row=0, padx=80,pady=(75,0))
-            self.next.configure(state= DISABLED)
             self.answ1.configure(bg="grey")
             self.answ2.configure(bg="grey")
 
-            self.antw1.grid(column=0, row=1,pady=(8,5))
-            self.antw2.grid(column=0, row=2,pady=5)
+
+
 
 
         else:
@@ -79,22 +74,19 @@ class Quiz:
     def SetResponse(self,value):
         if self.lock == False: 
             if value == 1:
-<<<<<<< Updated upstream
-                self.antw1.configure(bg="green")
-            else:
-                self.antw2.configure(bg="green")     
-=======
                 self.answ1.configure(bg="green")
-                self.next.configure (state= NORMAL)
             else:
-                self.answ2.configure(bg="green")  
-                self.next.configure (state= NORMAL)
->>>>>>> Stashed changes
+                self.answ2.configure(bg="green")     
             self.response = value
             self.nodes.append(value)
             self.lock = True
-            choice=None
 
+    def QuestionClearer(self):
+        if self.lock == True: 
+            self.answ1.configure(bg="gray")
+            self.answ2.configure(bg="gray")     
+            self.nodes.pop(-1)
+            self.lock = False
 
 class Menu:
     def __init__(self):
